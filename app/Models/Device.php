4ff324a5e61020a00bf8a1e7c8a48a9087e0d93f
Mithunraj <mbel001@mbel.co.jp>
2020-11-13 11:06:31 +0900

    Device model

---
+++ app/Models/Device.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Device extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'device';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'plate_number',
+        'scan_code',
+        'channel_number',
+        'group_name',
+        'tcp_server_addr',
+        'tcp_stream_out_port',
+        'udp_server_addr',
+        'udp_stream_out_port',
+        'net_type',
+        'device_type',
+        'create_time',
+        'update_time',
+        'is_active',
+        'stk_user',
+    ];
+}
