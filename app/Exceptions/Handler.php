4c4117ac53d6c7aa7387ccaf341400c309ab0d31
Pongpeera Kundilokborirak <pongpeera.kun@gmail.com>
2020-11-18 10:23:02 +0900

    added log statement at exception handler.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,10 @@
 namespace App\Exceptions;
 
 use Carbon\Exceptions\InvalidFormatException;
+use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Log;
 use InvalidArgumentException;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
 use Throwable;
@@ -46,11 +48,14 @@ class Handler extends ExceptionHandler
         || $exception instanceof ModelNotFoundException) {
             return response()->json(['message' => 'Data not found.'], 404);
         } else if ($exception instanceof InvalidFormatException) {
-            return response()->json(['message' => $exception->message], 500);
+            return response()->json(['message' => $exception->getMessage()], 500);
         } else if ($exception instanceof InvalidArgumentException) {
-            return response()->json(['message' => $exception->message], 400);
+            return response()->json(['message' => $exception->getMessage()], 400);
         } else if ($exception instanceof StonkamResultIsFailedException) {
-            return response()->json(['message' => $exception->message], 500);
+            return response()->json(['message' => $exception->getMessage()], 500);
+        } else if ($exception instanceof Exception) {
+            Log::error('unknown error please look up at previous line');
+            return response()->json(['message' => 'Something went wrong'], 500);
         }
 
         return parent::render($request, $exception);
