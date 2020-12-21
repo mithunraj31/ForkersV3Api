51d7757f5d679beaaf1492e76839161ac57d0f25
lasitha <mbel002@mbel.co.jp>
2020-12-21 11:18:37 +0900

    add role and customer info in index users

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -125,12 +125,12 @@ class UserService implements UserServiceInterface
     public function getAll($perPage=15)
     {
         if(AuthValidator::isAdmin()){
-            $users = new UserResourceCollection(User::with('owner')->paginate($perPage));
+            $users = new UserResourceCollection(User::with('owner', 'role', 'customer')->paginate($perPage));
             $users->withQueryString()->links();
             return $users;
         }else{
             $customer = Customer::where('stk_user',AuthValidator::getStkUser())->first();
-            $users = new UserResourceCollection(User::where('customer_id',$customer->id)->with('owner')->paginate($perPage));
+            $users = new UserResourceCollection(User::where('customer_id',$customer->id)->with('owner', 'role')->paginate($perPage));
             $users->withQueryString()->links();
             return $users;
         }
