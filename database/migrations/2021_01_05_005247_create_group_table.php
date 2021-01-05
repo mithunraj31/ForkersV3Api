89f27217e01c1d81bde5ea159efe7ab9995409bf
Pongpeera Kundilokborirak <pongpeera.kun@gmail.com>
2021-01-05 11:57:29 +0900

    improved migration files

--- database/migrations/2021_01_05_005247_create_group_table.php
+++ database/migrations/2021_01_05_005247_create_group_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRoleTable extends Migration
+class CreateGroupTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class CreateRoleTable extends Migration
      */
     public function up()
     {
-        Schema::create('role', function (Blueprint $table) {
-            $table->increments('id');
+        Schema::create('group', function (Blueprint $table) {
+            $table->id();
             $table->string('name')->nullable(false);
-            $table->string('description')->nullable(true);
-            $table->bigInteger('customer_id');
+            $table->longText('description')->nullable(true);
+            $table->bigInteger('parent_id')->nullable(true);
             $table->bigInteger('owner_id')->nullable(true);
-            $table->datetime('deleted_at');
+            $table->string('customer_id')->nullable(false);
+            $table->string('deleted_at')->nullable(true);
             $table->timestamps();
-
         });
     }
 
@@ -32,6 +32,6 @@ class CreateRoleTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('role');
+        Schema::dropIfExists('group');
     }
 }
